03fe6a6ffa5c7766bce9413fa379cea1ba802cc3
Andrij Demianczuk <andrij.demianczuk@databricks.com>
2023-10-30 16:00:41 -0600

    testing spark and databricks sdk config

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+from databricks.connect import DatabricksSession
+from databricks.sdk import WorkspaceClient
+# from databricks.sdk.runtime import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    spark = DatabricksSession.builder.profile("ml-1").getOrCreate()
+    #
+    # df = spark.table("ademianczuk.hls.b_callout")
+    # df.groupBy("Facility").count().show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    w = WorkspaceClient(profile="ml-1")
+    dbutils = w.dbutils
+
+    files_in_root = dbutils.fs.ls('/FileStore/Users/andrij.demianczuk@databricks.com/')
+    for i in (dbutils.fs.ls("/")):
+        print(i)
