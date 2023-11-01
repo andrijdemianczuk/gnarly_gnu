222871392dc3fff33c6c135f6289c4bba4c38b57
andrijdemianczuk <andrijdemianczuk>
2023-11-01 21:25:28 +0000

    added to the notebooks directory

--- Notebooks/Exploration.py
+++ Notebooks/Exploration.py
@@ -3,4 +3,10 @@ dbutils.fs.ls("/FileStore/Users/andrij.demianczuk@databricks.com/tmp")
 
 # COMMAND ----------
 
+current_user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
+dbutils.fs.ls(f"/Users/{current_user}/")
 
+# COMMAND ----------
+
+# Create the working directory if not exists. If it exists, nothing will happen
+dbutils.fs.mkdirs(f"/Users/{current_user}/data")
