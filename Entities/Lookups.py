2804732d76d51bc8b05c591bcc8b8e4d0f692e33
Andrij Demianczuk <andrij.demianczuk@databricks.com>
2023-11-07 09:51:40 -0700

    First draft of Generator Logic

---
+++ Entities/Lookups.py
@@ -0,0 +1,36 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+class Lookups:
+
+    def __int__(self,  spark, val: int = 0):
+        self.val = val
+        self.spark = spark
+
+    def defineStruct(self, schema):
+        print(self.val)
+        print(schema)
+
+    def generateAirpots(self, spark, current_user):
+        data = [("YYC","Calgary","AB"),
+                ("YEG","Edmonton","AB"),
+                ("YFC","Fredericton","NB"),
+                ("YQX","Gander","NFL"),
+                ("YHZ","Halifax","NS"),
+                ("YQM","Moncton","NB"),
+                ("YUL","Montreal","QC"),
+                ("YOW","Ottawa","ON"),
+                ("YQB","Quebec City","QC"),
+                ("YYT","Saint Johns","NFL"),
+                ("YYZ","Toronto","On"),
+                ("YVR","Vancouver","BC"),
+                ("YWG","Winnipeg","MB")]
+
+        schema = StructType([ StructField("IATA",StringType(), True),
+                              StructField("City",StringType(), True),
+                              StructField("Province", StringType(), True)])
+
+        df = spark.createDataFrame(data=data, schema=schema)
+        df.show()
+
+        # df.write.format('delta').mode('overwrite').save(f"/Users/{current_user}/data/airlines/baggage/lookups")
+
--- main.py
+++ main.py
@@ -1,49 +1,39 @@
-import faker
+# Databricks Imports
 from databricks.connect import DatabricksSession
 from databricks.sdk import WorkspaceClient
 from datetime import datetime, date, time, timezone, timedelta
 from databricks.sdk.runtime import *
-from Entities.User import User
+
+# Faker / Random Imports
 from faker import Faker
 import random
 from faker.providers import BaseProvider as fake
 
-import Entities.User
+# Entity Imports
 from Entities.User import User
+from Entities.Lookups import Lookups
 
 if __name__ == '__main__':
-    # spark = DatabricksSession.builder.profile("ml-1").getOrCreate()
-    # w = WorkspaceClient(profile="ml-1")
-    # dbutils = w.dbutils
+    spark = DatabricksSession.builder.profile("ml-1").getOrCreate()
+    w = WorkspaceClient(profile="ml-1")
+    dbutils = w.dbutils
 
-
-    #Initialize values and settings
-    # current_user = User.getCurrent(spark)
+    # Initialize values and settings
+    current_user = User.getCurrent(spark=spark)
     now = datetime.now(tz=timezone.utc)
     then = datetime.now(tz=timezone.utc) - timedelta(hours=1)
     windowStart = datetime.strptime(then.strftime('%Y-%m-%d %H:00:00'), '%Y-%m-%d %H:%M:%S')
     windowEnd = datetime.strptime(then.strftime('%Y-%m-%d %H:59:59'), '%Y-%m-%d %H:%M:%S')
 
-    print(now)
-    print(windowStart)
-    print(windowEnd)
+    # Make the directories if they don't exist. This will be relative for each user who runs this
+    dbutils.fs.mkdirs(f"/Users/{current_user}/data/airlines/baggage")
+    dbutils.fs.mkdirs(f"/Users/{current_user}/data/airlines/baggage/lookups")
+    dbutils.fs.mkdirs(f"/Users/{current_user}/data/airlines/baggage/flights")
+    dbutils.fs.mkdirs(f"/Users/{current_user}/data/airlines/baggage/bagtracking")
 
-    # Make the directory if it doesn't exist. This will be relative for each user who runs this
-    # dbutils.fs.mkdirs(f"/Users/{current_user}/data/airlines/baggage")
+    for i in (dbutils.fs.ls(f"/Users/{current_user}/data/airlines/baggage")):
+        print(i)
 
-    # df_medunits = spark.table("ademianczuk.hls.l_nc_medunits").limit(5)
-    # medunits = df_medunits.select(df_medunits.Short).rdd.flatMap(lambda x: x).collect()
-    #reason = ['Sick', 'Family Emergency', 'No-Show', 'Vacation', 'Training', 'PTO']
-    # df_medunits.show()
-
-    # df_medunits.write.parquet(path=f"/Users/{current_user}/data/airlines/baggage/test.parquet")
-
-    # for i in (dbutils.fs.ls(f"/Users/{current_user}/data/airlines/baggage")):
-    #     print(i)
-    # Faker.seed(0)
-    # for _ in range(5):
-    #     fake.lexify(text='Random Identifier: ??????????')
-
-
-
+    # Create the lookup tables for airports
+    Lookups().generateAirpots(spark=spark)
 
