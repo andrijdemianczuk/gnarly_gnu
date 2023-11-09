a6018c709607df03929608a55f4145792a755894
Andrij Demianczuk <andrij.demianczuk@databricks.com>
2023-11-09 15:46:12 -0700

    Fixed column label for destination

--- Generator/Generate.py
+++ Generator/Generate.py
@@ -5,6 +5,8 @@ import faker
 from faker import Faker
 from faker_airtravel import AirTravelProvider
 
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
 class Generate:
 
     def __init__(self, now, windowStart, windowEnd, spark):
@@ -13,29 +15,46 @@ class Generate:
         self.windowEnd = windowEnd
         self.spark = spark
 
-    def generateFlights(self):
+    def generateFlights(self, catalog="ademianczuk", database="flights"):
         try:
             print("generating flights...")
 
-            #Build the first faker object and pass in the airtravel provider (https://pypi.org/project/faker_airtravel/)
+            # Build the first faker object and pass in the airtravel provider
+            # https://pypi.org/project/faker_airtravel/
             fake = Faker()
             fake.add_provider(AirTravelProvider)
 
+            # Build a list of available destinations
             df = self.spark.table("ademianczuk.flights.canada_iata_codes").select("IATA")
             df = df.filter(df.IATA != "YYC")
             iata_list = list(df.select('IATA').
                        toPandas()['IATA'])
 
-            # Generate flights and add them to the output dataframe
+            # Define the structure of the dataframe
+            schema = StructType([StructField("Flight_Number", StringType(), True),
+                                 StructField("Destination", StringType(), True),
+                                 StructField("Departure_Time", StringType(), True),
+                                 StructField("Cutoff_Time", StringType(), True)])
+
+            # Create the loop parameters
             mins = 0 # offset minutes
+            schedule = []
+
+            # Iterate and build the schedule itinerary
             for _ in range(4):
                 flight_num = f"WS1{random.randint(111, 999)}"
-                departure_time = self.windowEnd - timedelta(minutes=30+mins)
-                cutoff_time = departure_time - timedelta(minutes=10)
-                airport = random.choice(iata_list)
+                departure_time = self.windowEnd - timedelta(minutes=15+mins)
+                cutoff_time = departure_time - timedelta(minutes=15)
+                destination = random.choice(iata_list)
                 mins += 5
+                flight = (flight_num, destination, departure_time, cutoff_time)
+                schedule.append(flight)
 
-                print(f"Flight number {flight_num} to {airport} leaves at {departure_time}. Bag loading cutoff is {cutoff_time}")
+            df = self.spark.createDataFrame(data=schedule, schema=schema)
+            df.show()
+
+            df.write.format('delta').option("mergeSchema", True).mode('overwrite').saveAsTable(
+                f"{catalog}.{database}.flight_schedule")
 
         except Exception as e:
             print(e)
