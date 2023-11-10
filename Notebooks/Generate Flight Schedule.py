2e39d750aa7acd6bb85066b2354c8484123e2482
andrijdemianczuk <andrijdemianczuk>
2023-11-10 21:49:47 +0000

    migrated logic to notebooks

---
+++ Notebooks/Generate Flight Schedule.py
@@ -0,0 +1,58 @@
+# Databricks notebook source
+# DBTITLE 1,Imports
+from faker import Faker
+from faker_airtravel import AirTravelProvider
+from datetime import datetime, timedelta
+
+import pandas as pd
+import random
+import pytz
+
+# COMMAND ----------
+
+# DBTITLE 1,Initialize Variables
+fake = Faker()
+catalog = "ademianczuk"
+database = "flights"
+
+# COMMAND ----------
+
+# DBTITLE 1,Build the Airport IATA Code List
+airportsDF = spark.table(f"{catalog}.{database}.canada_iata_codes")
+airportsDF = airportsDF.filter(airportsDF.IATA != "YYC")
+iata_codes = airportsDF.select(airportsDF.IATA).rdd.flatMap(lambda x: x).collect()
+# iata_codes
+
+# COMMAND ----------
+
+# DBTITLE 1,Build the Pandas Dataframe
+flightsDF = pd.DataFrame(columns=("Flight_Number", "Destination", "Departure_Time", "Cutoff_Time"))
+
+now = datetime.now()
+then = datetime.now() - timedelta(hours=1)  # use now(tz=timezone.utc) for universal time
+windowStart = datetime.strptime(then.strftime('%Y-%m-%d %H:00:00'), '%Y-%m-%d %H:%M:%S')
+windowEnd = datetime.strptime(then.strftime('%Y-%m-%d %H:59:59'), '%Y-%m-%d %H:%M:%S')
+mins = 0
+
+for i in range(4):
+  flights = [
+    f"WS1{random.randint(111,999)}",
+    random.choice(iata_codes),
+    windowEnd - timedelta(minutes=15 + mins),
+    windowEnd - timedelta(minutes=30 + mins)
+  ]
+  flightsDF.loc[i] = [item for item in flights]
+  mins += 5
+
+flightsDF
+
+# COMMAND ----------
+
+# DBTITLE 1,Write to Delta
+fDF = spark.createDataFrame(flightsDF)
+fDF.coalesce(1)
+fDF.write.format('delta').option("mergeSchema", True).mode('append').saveAsTable(f"{catalog}.{database}.flight_schedule")
+
+# COMMAND ----------
+
+
---
+++ Notebooks/Generate IATA Codes.py
@@ -0,0 +1,60 @@
+# Databricks notebook source
+# DBTITLE 1,Import Libraries
+# Imports
+from pyspark.sql.functions import *
+from datetime import datetime, timedelta
+
+# COMMAND ----------
+
+# DBTITLE 1,Initialize the Variables
+current_user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
+now = current_timestamp()
+then = now + expr('INTERVAL -1 HOURS')
+catalog = "ademianczuk"
+database = "flights"
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Define the Lookup Table
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+class Lookups:
+
+    def __int__(self, val: int = 0):
+        self.val = val
+
+    def defineStruct(self, schema):
+        print(self.val)
+        print(schema)
+
+    def generateAirpots(self, current_user, catalog="ademianczuk", database="flights"):
+        data = [("YYC","Calgary","AB"),
+                ("YEG","Edmonton","AB"),
+                ("YFC","Fredericton","NB"),
+                ("YQX","Gander","NFL"),
+                ("YHZ","Halifax","NS"),
+                ("YQM","Moncton","NB"),
+                ("YUL","Montreal","QC"),
+                ("YOW","Ottawa","ON"),
+                ("YQB","Quebec City","QC"),
+                ("YYT","Saint Johns","NFL"),
+                ("YYZ","Toronto","On"),
+                ("YVR","Vancouver","BC"),
+                ("YWG","Winnipeg","MB")]
+
+        schema = StructType([ StructField("IATA",StringType(), True),
+                              StructField("City",StringType(), True),
+                              StructField("Province", StringType(), True)])
+
+        df = spark.createDataFrame(data=data, schema=schema)
+        # df.show()
+
+        df.write.format('delta').option("mergeSchema", True).mode('overwrite').saveAsTable(f"{catalog}.{database}.canada_iata_codes")
+
+# COMMAND ----------
+
+# DBTITLE 1,Write the Lookup Table
+if not (spark.catalog.tableExists(f"{catalog}.{database}.canada_iata_codes")):
+  lk = Lookups()
+  lk.generateAirpots(current_user=current_user, catalog=catalog, database=database)
